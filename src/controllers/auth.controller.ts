23693a4739e15f0d00a145e6189a96b3ee4b54af
wpcodevo <ziddah.edem@gmail.com>
2023-07-24 19:24:43 +0000

    updated

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,40 +1,40 @@
-import config from 'config';
-import { CookieOptions, NextFunction, Request, Response } from 'express';
-import { CreateUserInput, LoginUserInput } from '../schema/user.schema';
+import config from "config";
+import { CookieOptions, NextFunction, Request, Response } from "express";
+import { CreateUserInput, LoginUserInput } from "../schema/user.schema";
 import {
   createUser,
   findUser,
   findUserById,
   signToken,
-} from '../services/user.service';
-import AppError from '../utils/appError';
-import redisClient from '../utils/connectRedis';
-import { signJwt, verifyJwt } from '../utils/jwt';
+} from "../services/user.service";
+import AppError from "../utils/appError";
+import redisClient from "../utils/connectRedis";
+import { signJwt, verifyJwt } from "../utils/jwt";
 
 // Exclude this fields from the response
-export const excludedFields = ['password'];
+export const excludedFields = ["password"];
 
 // Cookie options
 const accessTokenCookieOptions: CookieOptions = {
   expires: new Date(
-    Date.now() + config.get<number>('accessTokenExpiresIn') * 60 * 1000
+    Date.now() + config.get<number>("accessTokenExpiresIn") * 60 * 1000
   ),
-  maxAge: config.get<number>('accessTokenExpiresIn') * 60 * 1000,
+  maxAge: config.get<number>("accessTokenExpiresIn") * 60 * 1000,
   httpOnly: true,
-  sameSite: 'lax',
+  sameSite: "lax",
 };
 
 const refreshTokenCookieOptions: CookieOptions = {
   expires: new Date(
-    Date.now() + config.get<number>('refreshTokenExpiresIn') * 60 * 1000
+    Date.now() + config.get<number>("refreshTokenExpiresIn") * 60 * 1000
   ),
-  maxAge: config.get<number>('refreshTokenExpiresIn') * 60 * 1000,
+  maxAge: config.get<number>("refreshTokenExpiresIn") * 60 * 1000,
   httpOnly: true,
-  sameSite: 'lax',
+  sameSite: "lax",
 };
 
 // Only set secure to true in production
-if (process.env.NODE_ENV === 'production')
+if (process.env.NODE_ENV === "production")
   accessTokenCookieOptions.secure = true;
 
 export const registerHandler = async (
@@ -50,7 +50,7 @@ export const registerHandler = async (
     });
 
     res.status(201).json({
-      status: 'success',
+      status: "success",
       data: {
         user,
       },
@@ -58,8 +58,8 @@ export const registerHandler = async (
   } catch (err: any) {
     if (err.code === 11000) {
       return res.status(409).json({
-        status: 'fail',
-        message: 'Email already exist',
+        status: "fail",
+        message: "Email already exist",
       });
     }
     next(err);
@@ -80,23 +80,23 @@ export const loginHandler = async (
       !user ||
       !(await user.comparePasswords(user.password, req.body.password))
     ) {
-      return next(new AppError('Invalid email or password', 401));
+      return next(new AppError("Invalid email or password", 401));
     }
 
     // Create the Access and refresh Tokens
     const { access_token, refresh_token } = await signToken(user);
 
     // Send Access Token in Cookie
-    res.cookie('access_token', access_token, accessTokenCookieOptions);
-    res.cookie('refresh_token', refresh_token, refreshTokenCookieOptions);
-    res.cookie('logged_in', true, {
+    res.cookie("access_token", access_token, accessTokenCookieOptions);
+    res.cookie("refresh_token", refresh_token, refreshTokenCookieOptions);
+    res.cookie("logged_in", true, {
       ...accessTokenCookieOptions,
       httpOnly: false,
     });
 
     // Send Access Token
     res.status(200).json({
-      status: 'success',
+      status: "success",
       access_token,
     });
   } catch (err: any) {
@@ -106,9 +106,9 @@ export const loginHandler = async (
 
 // Refresh tokens
 const logout = (res: Response) => {
-  res.cookie('access_token', '', { maxAge: 1 });
-  res.cookie('refresh_token', '', { maxAge: 1 });
-  res.cookie('logged_in', '', {
+  res.cookie("access_token", "", { maxAge: 1 });
+  res.cookie("refresh_token", "", { maxAge: 1 });
+  res.cookie("logged_in", "", {
     maxAge: 1,
   });
 };
@@ -125,9 +125,9 @@ export const refreshAccessTokenHandler = async (
     // Validate the Refresh token
     const decoded = verifyJwt<{ sub: string }>(
       refresh_token,
-      'refreshTokenPublicKey'
+      "refreshTokenPublicKey"
     );
-    const message = 'Could not refresh access token';
+    const message = "Could not refresh access token";
     if (!decoded) {
       return next(new AppError(message, 403));
     }
@@ -146,20 +146,20 @@ export const refreshAccessTokenHandler = async (
     }
 
     // Sign new access token
-    const access_token = signJwt({ sub: user._id }, 'accessTokenPrivateKey', {
-      expiresIn: `${config.get<number>('accessTokenExpiresIn')}m`,
+    const access_token = signJwt({ sub: user._id }, "accessTokenPrivateKey", {
+      expiresIn: `${config.get<number>("accessTokenExpiresIn")}m`,
     });
 
     // Send the access token as cookie
-    res.cookie('access_token', access_token, accessTokenCookieOptions);
-    res.cookie('logged_in', true, {
+    res.cookie("access_token", access_token, accessTokenCookieOptions);
+    res.cookie("logged_in", true, {
       ...accessTokenCookieOptions,
       httpOnly: false,
     });
 
     // Send response
     res.status(200).json({
-      status: 'success',
+      status: "success",
       access_token,
     });
   } catch (err: any) {
@@ -174,9 +174,9 @@ export const logoutHandler = async (
 ) => {
   try {
     const user = res.locals.user;
-    await redisClient.del(user._id);
+    await redisClient.del(String(user._id));
     logout(res);
-    return res.status(200).json({ status: 'success' });
+    return res.status(200).json({ status: "success" });
   } catch (err: any) {
     next(err);
   }
